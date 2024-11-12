a6ab3937545ee3343fa4868c4732d604b7687aed
ana007nasik <anastasiamiam0@gmail.com>
2024-11-12 10:35:00 +0100

    try

--- snake.py
+++ snake.py
@@ -14,3 +14,34 @@ white = pygame.Color(255, 255, 255)
 red = pygame.Color(255, 0, 0)
 green = pygame.Color(0, 255, 0)
 blue = pygame.Color(0, 0, 255)
+
+#That will be used further in the main logic of the game to change the speed of the snake :)
+# Initialising pygame
+pygame.init()
+
+# Initialise game window
+pygame.display.set_caption('GeeksforGeeks Snakes')
+game_window = pygame.display.set_mode((window_x, window_y))
+
+# FPS (frames per second) controller
+fps = pygame.time.Clock()
+
+# defining snake default position 
+snake_position = [100, 50]
+
+# defining first 4 blocks of snake
+# body
+snake_body = [  [100, 50],
+                [90, 50],
+                [80, 50],
+                [70, 50]
+            ]
+# fruit position 
+fruit_position = [random.randrange(1, (window_x//10)) * 10,
+                  random.randrange(1, (window_y//10)) * 10]
+fruit_spawn = True
+
+# setting default snake direction 
+# towards rightx
+direction = 'RIGHT'
+change_to = direction
